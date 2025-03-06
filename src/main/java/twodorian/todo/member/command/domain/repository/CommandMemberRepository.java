ef6a03e7f750a7ebf757d434070bd1ae34c9e3eb
INUK-ai <arheon2469@gmail.com>
2025-03-06 20:51:12 +0900

    Feat: CommandMemberRepository - findByEmail

--- src/main/java/twodorian/todo/member/command/domain/repository/CommandMemberRepository.java
+++ src/main/java/twodorian/todo/member/command/domain/repository/CommandMemberRepository.java
@@ -4,6 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import twodorian.todo.member.command.domain.model.Member;
 
+import java.util.Optional;
+
 @Repository
 public interface CommandMemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByEmail(String email);
 }
