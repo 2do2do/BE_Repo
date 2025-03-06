5a282c0f798ef6deb68b0e7a6e7b104f35729d99
INUK-ai <arheon2469@gmail.com>
2025-03-06 20:24:31 +0900

    Refactor: Member - 도메인 User 와 security User 구분을 위해 Member 로 이름 변경

---
+++ src/main/java/twodorian/todo/member/command/domain/model/Member.java
@@ -0,0 +1,38 @@
+package twodorian.todo.member.command.domain.model;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import twodorian.todo.member.command.domain.model.property.Authority;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "user_tb")
+public class Member {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+    @Column
+    private String email;
+    @Column
+    private String password;
+
+    @Enumerated(value = EnumType.STRING)
+    @ColumnDefault("'USER'")
+    private Authority authority;
+
+    @Builder
+    public Member(String name, String email, String password, Authority authority) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.authority = authority;
+    }
+}
---
+++ src/main/java/twodorian/todo/member/command/domain/model/property/Authority.java
@@ -0,0 +1,5 @@
+package twodorian.todo.member.command.domain.model.property;
+
+public enum Authority {
+    ADMIN, USER, GUEST, NONE
+}
